29986ca69dbd3627c5f59e353aa2f12742691407
vemundss <vemund@live.com>
2021-11-24 15:52:37 +0100

    Adds functional recurrent forwarding (grid cells)

--- src/CANN.py
+++ src/CANN.py
@@ -33,18 +33,21 @@ def grid_cell(f=1, orientation_offset=0, degrees=True, **kwargs):
     ks *= 2 * np.pi  # spatial angular frequency (unit-movement in space is one period)
     ks *= f  # user-defined spatial frequency
 
-    def wrapper(r):
+    def grid_cell_fn(r):
         # shape of r: (n**2,n**2,2)
         L = np.sqrt(r.shape[0])
+        # return shape: (n**2,n**2)
         return np.sum(np.cos((r @ ks.T) / L), axis=-1)
 
-    return wrapper
+    return grid_cell_fn
 
 
 class CANN:
     def __init__(self, Ng=4096, Np=512, nonlinearity="relu", **kwargs):
         self.Ng, self.Np = Ng, Np  # Ng should be a quadratic number
         n = int(np.sqrt(Ng))
+
+        # Create neural sheets (just imagine: topographically arranged cells)
         # shape: (n,n,2)
         self.neural_sheet_2d = np.stack(
             np.meshgrid(np.linspace(0, n - 1, n), np.linspace(0, n - 1, n)), axis=-1
@@ -53,10 +56,23 @@ class CANN:
         self.neural_sheet_1d = self.neural_sheet_2d.reshape(
             np.prod(self.neural_sheet_2d.shape[:-1]), self.neural_sheet_2d.shape[-1]
         )
+        
+        # set nonlinearity from argument
+        if nonlinearity is None:
+            self.nonlinearity = lambda x: x # identity/no activation
+        elif nonlinearity == "relu":
+            self.nonlinearity = lambda x: x * (x > 0) # ReLu activation
+        else:
+            raise NotImplementedError(f"{nonlinearity=} not implemented")
+        
+        # initialize weights
+        self.grid_cell_fn = grid_cell(**kwargs)
         # shape: (n**2,2)
         self.M = self._init_input_weights(self.neural_sheet_1d)
-
-        self.grid_cell_fn = grid_cell(**kwargs)
+        # shape: (n**2,n**2)
+        self.J = self._init_recurrent_weights(self.neural_sheet_1d, self.M, self.grid_cell_fn)
+        # shape: (n**2)
+        self.bias = np.zeros(Ng)
 
     def _init_input_weights(self, neural_sheet_1d):
         """
@@ -68,19 +84,31 @@ class CANN:
         return M  # shape=(n**2,2)
 
     def _init_recurrent_weights(self, neural_sheet_1d, beta, grid_cell_fn):
-        # (n**2,2,1) - (1,2,n**2) => (n**2,2,n**2)
-        old = neural_sheet_1d[..., None] - neural_sheet_1d.T[None]
-
         # (n**2,1,2) - (1,n**2,2) - (1,n**2,2)=> (n**2,n**2,2)
         W = neural_sheet_1d[:, None] - neural_sheet_1d[None] - beta[None]
-
-        # CAN DELETE OLD AND PRINT STUFF IF CORRECT
-        print(
-            "_init_recurrent_weights methods are same if zero: ", np.sum(abs(old - W))
-        )
         return grid_cell_fn(W)
 
     def _init_readout_weights(self):
+        pass
+
+    def g(self, h0, vs):
+        """
+        Iterate dynamics for a set of velocities (vs) and an intial state (h_0)
+
+        Parameters
+        ----------
+        h0 : np.ndarray
+            1D-array of length Ng. initial state of system
+        vs : np.ndarray
+            2D-array of shape (T,2)
+        """
+        hn = h0
+        for v in vs:
+             hn = self.nonlinearity(self.J @ hn + self.J @ (self.M @ v) + self.bias) 
+             hn /= np.sum(hn)
+        return hn 
+
+    def p(self):
         pass
 
     def forward(self, N):
