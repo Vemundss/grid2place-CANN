46b5f2cfb6aa417b5f423682108f8e8ebb6d1c4b
vemundss <vemund@live.com>
2021-11-25 16:04:54 +0100

    Minor logic clean-up. Also adds place cell forwarding

--- src/CANN.py
+++ src/CANN.py
@@ -1,7 +1,31 @@
+from typing import Callable
 import numpy as np
 
 
-def rotation_matrix(theta, degrees=True):
+def rotation_matrix(theta, degrees=True, **kwargs) -> np.ndarray:
+    """
+    Creates a 2D rotation matrix for theta
+
+    Parameters
+    ----------
+    theta : float
+        angle offset wrt. the cardinal x-axis
+    degrees : boolean
+        Whether to use degrees or radians
+
+    Returns
+    -------
+    rotmat : np.ndarray
+        the 2x2 rotation matrix
+
+    Examples
+    --------
+    >>> np.import numpy as np
+    >>> x = np.ones(2) / np.sqrt(2)
+    >>> rotmat = rotation_matrix(45)
+    >>> rotmax @ x
+    array([1. 0.])
+    """
     """
     2d-rotation matrix implementation
     """
@@ -11,19 +35,42 @@ def rotation_matrix(theta, degrees=True):
     return np.array(((c, -s), (s, c)))
 
 
-def grid_cell(f=1, orientation_offset=0, degrees=True, **kwargs):
+def grid_cell(phase_offset=0, orientation_offset=0, f=1, **kwargs) -> Callable:
     """
-    Implementation following the logic described in the paper:
-    From grid cells to place cells: A mathematical model
-    - Moser & Einevoll
+    Grid cell pattern constructed from three interacting 2D (plane) vectors
+    with 60 degrees relative orientational offsets.
+    See e.g. the paper: "From grid cells to place cells: A mathematical model"
+    - Moser & Einevoll 2005
 
-    Usage:
-        Set static params for a grid cell as: gc = grid_cell(*args)
-        Then calculate grid cell response wrt. pos=(x,y) as gc(pos)
+    Parameters
+    ----------
+    phase_offset : float
+        First plane vector is default along the cardinal x-axis. Phase-offset
+        turns this plane vector counter clockwise (default in degrees, but
+        can use **kwargs - degrees=False to use radians)
+    orientation_offset : np.ndarray
+        2D-array. Spatial (vector) phase offset of the grid pattern. Note that
+        the grid pattern is np.array([f,f])-periodic, so phase-offsets are also
+        f-periodic
+    f : float
+        Spatial frequency / periodicity. f=1 makes the grid cell unit-periodic
+
+    Returns
+    -------
+    grid_cell_fn : function
+        A grid cell function which can be evaluated at locations r
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> x = np.zeros(2)
+    >>> gc = grid_cell()
+    >>> gc(x)
+    2.0
     """
     rot_theta = 60  # degrees
     relative_R = rotation_matrix(rot_theta)
-    init_R = rotation_matrix(orientation_offset, degrees)
+    init_R = rotation_matrix(orientation_offset, **kwargs)
 
     k1 = np.array([1.0, 0.0])  # init wave vector. unit length in x-direction
     k1 = init_R @ k1
@@ -35,63 +82,91 @@ def grid_cell(f=1, orientation_offset=0, degrees=True, **kwargs):
 
     def grid_cell_fn(r):
         # shape of r: (n**2,n**2,2)
-        L = np.sqrt(r.shape[0])
         # return shape: (n**2,n**2)
-        return np.sum(np.cos((r @ ks.T) / L), axis=-1)
+        return np.sum(np.cos(r @ ks.T), axis=-1)
 
     return grid_cell_fn
 
 
 class CANN:
-    def __init__(self, Ng=4096, Np=512, nonlinearity="relu", **kwargs):
+    def __init__(self, Ng=4096, Np=512, nonlinearity="relu", Wp_mask=128, **kwargs):
         self.Ng, self.Np = Ng, Np  # Ng should be a quadratic number
-        n = int(np.sqrt(Ng))
+        self.L = int(np.sqrt(Ng))
+        self.Wp_mask = Wp_mask # number of grid cell - place cell connections
+        if self.L ** 2 != Ng:
+            raise UserWarning(f"Non-square Ng not supported: {self.L**2 != Ng=}")
 
         # Create neural sheets (just imagine: topographically arranged cells)
         # shape: (n,n,2)
         self.neural_sheet_2d = np.stack(
-            np.meshgrid(np.linspace(0, n - 1, n), np.linspace(0, n - 1, n)), axis=-1
+            np.meshgrid(
+                np.linspace(0, self.L - 1, self.L), np.linspace(0, self.L - 1, self.L)
+            ),
+            axis=-1,
         )
         # shape: (n**2,2)
         self.neural_sheet_1d = self.neural_sheet_2d.reshape(
             np.prod(self.neural_sheet_2d.shape[:-1]), self.neural_sheet_2d.shape[-1]
         )
-        
+
         # set nonlinearity from argument
         if nonlinearity is None:
-            self.nonlinearity = lambda x: x # identity/no activation
+            self.nonlinearity = lambda x: x  # identity/no activation
         elif nonlinearity == "relu":
-            self.nonlinearity = lambda x: x * (x > 0) # ReLu activation
+            self.nonlinearity = lambda x: x * (x > 0)  # ReLu activation
         else:
             raise NotImplementedError(f"{nonlinearity=} not implemented")
-        
+
         # initialize weights
         self.grid_cell_fn = grid_cell(**kwargs)
         # shape: (n**2,2)
         self.M = self._init_input_weights(self.neural_sheet_1d)
         # shape: (n**2,n**2)
-        self.J = self._init_recurrent_weights(self.neural_sheet_1d, self.M, self.grid_cell_fn)
+        self.J = self._init_recurrent_weights(
+            self.neural_sheet_1d, self.M, self.grid_cell_fn
+        )
         # shape: (n**2)
-        self.bias = np.zeros(Ng)
+        self.Wp = self._init_readout_weights()
+        self.bias = np.zeros(self.Ng)
 
-    def _init_input_weights(self, neural_sheet_1d):
+    def _init_input_weights(self, neural_sheet_1d) -> np.ndarray:
         """
-        Sorscher initialization - NESW, i.e. cardinal diretions
+        Initialise input weights
 
+        Follows Sorschers initialization - NESW, i.e. cardinal directions.
         M can be considered Beta in Sorscher (shift-matrix or smt).
+
+        Parameters
+        ----------
+        neural_sheet_1d : np.ndarray
+            2D-array of shape (Ng,2). 1D-representation of a 2D neural sheet
+            of the topographical arrangement of neurons.
+
+        Returns
+        -------
+        M : np.ndarray
+            2D-array of shape (Ng,2)
         """
         M = (neural_sheet_1d[:, ::-1] % 2) * (-1) ** neural_sheet_1d
-        return M  # shape=(n**2,2)
+        return M
 
     def _init_recurrent_weights(self, neural_sheet_1d, beta, grid_cell_fn):
         # (n**2,1,2) - (1,n**2,2) - (1,n**2,2)=> (n**2,n**2,2)
         W = neural_sheet_1d[:, None] - neural_sheet_1d[None] - beta[None]
-        return grid_cell_fn(W)
+        W /= self.L  # relate periodicity to the length of the neural sheet
+        orientation_offsets = 2*np.pi*np.random.random(self.Ng)
+        W = np.array([grid_cell(orientation_offset=orientation_offsets[i], degrees=False)(W[i]) for i in range(self.Ng)])
+        return W
+        #return grid_cell_fn(W)
 
     def _init_readout_weights(self):
-        pass
+        Wp = np.zeros((self.Np,self.Ng))
+        for i in range(self.Np):
+            connectivity_idxs = np.random.choice(np.arange(self.Ng), size=self.Wp_mask, replace=False)
+            Wp[i,connectivity_idxs] = 1
+        return Wp
 
-    def g(self, h0, vs):
+    def g(self, h0, vs) -> np.ndarray:
         """
         Iterate dynamics for a set of velocities (vs) and an intial state (h_0)
 
@@ -101,24 +176,25 @@ class CANN:
             1D-array of length Ng. initial state of system
         vs : np.ndarray
             2D-array of shape (T,2)
+
+        Returns
+        -------
+        hn : np.ndarray
+            1D-array of shape (Ng). of the state of the system after all 
+            velocities (vs) have been sequentially applied
         """
         hn = h0
         for v in vs:
-             hn = self.nonlinearity(self.J @ hn + self.J @ (self.M @ v) + self.bias) 
-             hn /= np.sum(hn)
-        return hn 
+            hn = self.nonlinearity(self.J @ hn + self.J @ (self.M @ v) + self.bias)
+            hn /= np.sum(hn)
+        return hn
 
-    def p(self):
-        pass
+    def p(self, g_inputs):
+        return self.Wp @ g_inputs
 
-    def forward(self, N):
-        for i in range(N):
-            # ...step
-            continue
-        pass
-
-    def _step(self):
-        pass
+    def forward(self, h0, vs):
+        gs = self.g(h0, vs)
+        return self.p(gs)
 
     def prune_mask():
         """Set a prune mask"""
